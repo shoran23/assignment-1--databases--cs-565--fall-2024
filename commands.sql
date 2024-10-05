c8b55ee1316932f31ec6f0ba33dacac835f3bdc8
shoran23 <scottf.horan@gmail.com>
2024-10-05 14:13:04 -0400

    completed additional commands

--- commands.sql
+++ commands.sql
@@ -1,16 +1,95 @@
 -- Get the name of the store and its URL where any spices were purchased.
+SELECT DISTINCT
+    store_name,
+    url_address
+FROM
+    stores
+JOIN
+    store_addresses
+    USING(store_id)
+JOIN
+    spice_purchases
+    USING(store_id);
+
+
+
+
+
 
 -- Get the name of the store and its URL where spices were purchased, including their barcode.
+SELECT DISTINCT
+    store_name,
+    url_address,
+    barcode
+FROM
+    stores
+JOIN
+    store_addresses
+    USING(store_id)
+JOIN
+    spice_purchases
+    USING(store_id);
+
+
+
 
 -- Get stores who only have an online address.
- SELECT store_name, store_id, comment FROM stores INNER JOIN store_addresses using (store_id) WHERE store_addresses.physical_address IS NULL;
+ SELECT
+    store_name,
+    store_id,
+    comment
+ FROM
+     stores
+JOIN
+    store_addresses
+    USING(store_id)
+ WHERE
+     store_addresses.physical_address IS NULL;
+
+
+
 
 -- Get stores who have an online and a physical address.
-SELECT store_name, store_id, comment FROM stores INNER JOIN store_addresses using (store_id) WHERE store_addresses.physical_address IS NOT NULL AND store_addresses.url_address IS NOT NULL;
+SELECT
+    store_name,
+    store_id,
+    comment
+FROM
+    stores
+JOIN
+    store_addresses
+    USING(store_id)
+WHERE
+    store_addresses.physical_address IS NOT NULL AND store_addresses.url_address IS NOT NULL;
+
+
+
+
 
 -- Get spices whose names are also brands. For example, Fresh Direct is a store that also makes spices.
+SELECT DISTINCT
+    store_name,
+    brand
+FROM
+    stores
+JOIN
+    spices
+WHERE
+    store_name=brand;
+
+
+
 
 -- Get spices whose names contain “cinnamon”.
-SELECT * FROM spices WHERE UPPER(SPICE_NAME) LIKE UPPER("%cinnamon%");
+SELECT
+    *
+FROM
+    spices
+WHERE UPPER(SPICE_NAME) LIKE UPPER("%cinnamon%");
+
+
+
+
 
 -- Get the brand name of the Sumac spice and the name and URL of the store where it was purchased.
+
--- testing-commands.sql
+++ testing-commands.sql
@@ -1,4 +1,6 @@
---SELECT physical_address, url_address FROM store_addresses INNER JOIN stores USING (store_id);
---select store_name from stores inner join store_addresses using (store_id) where store_addresses.physical_address='675 6th Ave, New York, NY 10010';
-
-SELECT store_name, store_id, comment FROM stores INNER JOIN store_addresses using (store_id) WHERE store_addresses.physical_address IS NULL;
+SELECT
+    brand
+FROM
+    spices
+WHERE
+    spice_name="Sumac";
